13b7fdfe0645fc94fc1ce75cb6fc0983ba960250
prashant45667 <prashantgond724@gmail.com>
2026-08-01 17:00:00 +0000

    Improve responsive margins on tablet view (patch #4)

--- frontend/src/utils/logger.ts
+++ frontend/src/utils/logger.ts
@@ -2,3 +2,4 @@
 export const log_1 = () => console.log("Init 1");
 export const log_2 = () => console.log("Init 2");
 export const log_3 = () => console.log("Init 3");
+export const log_4 = () => console.log("Init 4");
