64ba777152d5958a6f74f607eb7b5bae124a4294
prashant45667 <prashantgond724@gmail.com>
2026-08-02 12:00:00 +0000

    Enhance UI typography and font weights (patch #12)

--- frontend/src/utils/logger.ts
+++ frontend/src/utils/logger.ts
@@ -10,3 +10,4 @@ export const log_8 = () => console.log("Init 8");
 export const log_9 = () => console.log("Init 9");
 export const log_10 = () => console.log("Init 10");
 export const log_11 = () => console.log("Init 11");
+export const log_12 = () => console.log("Init 12");
