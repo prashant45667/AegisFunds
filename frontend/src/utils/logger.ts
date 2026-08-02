7c4bdcd0d2acf1c278220ed3e577df0cd499d672
prashant45667 <prashantgond724@gmail.com>
2026-08-02 02:00:00 +0000

    Refactor WalletConnect component for better error handling (patch #7)

--- frontend/src/utils/logger.ts
+++ frontend/src/utils/logger.ts
@@ -5,3 +5,4 @@ export const log_3 = () => console.log("Init 3");
 export const log_4 = () => console.log("Init 4");
 export const log_5 = () => console.log("Init 5");
 export const log_6 = () => console.log("Init 6");
+export const log_7 = () => console.log("Init 7");
