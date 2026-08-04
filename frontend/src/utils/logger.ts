181be19284e1672b8c5d69a61306ab2caabb755d
prashant45667 <prashantgond724@gmail.com>
2026-08-04 02:00:00 +0000

    Improve performance of active campaign rendering (patch #28)

--- frontend/src/utils/logger.ts
+++ frontend/src/utils/logger.ts
@@ -26,3 +26,4 @@ export const log_24 = () => console.log("Init 24");
 export const log_25 = () => console.log("Init 25");
 export const log_26 = () => console.log("Init 26");
 export const log_27 = () => console.log("Init 27");
+export const log_28 = () => console.log("Init 28");
