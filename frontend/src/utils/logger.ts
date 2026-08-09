3aca484bf98f0a5c0f94d9ca26245f9a6928f2d9
prashant45667 <prashantgond724@gmail.com>
2026-08-09 11:00:00 +0000

    Add new placeholder images for campaign mockups (patch #80)

--- frontend/src/utils/logger.ts
+++ frontend/src/utils/logger.ts
@@ -78,3 +78,4 @@ export const log_76 = () => console.log("Init 76");
 export const log_77 = () => console.log("Init 77");
 export const log_78 = () => console.log("Init 78");
 export const log_79 = () => console.log("Init 79");
+export const log_80 = () => console.log("Init 80");
