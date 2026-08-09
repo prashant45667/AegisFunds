77ebf9c50e55f19a4eda2b25a31469c18f7546b2
prashant45667 <prashantgond724@gmail.com>
2026-08-09 04:00:00 +0000

    Fix styling issues in CampaignFeed grid (patch #77)

--- frontend/src/utils/logger.ts
+++ frontend/src/utils/logger.ts
@@ -75,3 +75,4 @@ export const log_73 = () => console.log("Init 73");
 export const log_74 = () => console.log("Init 74");
 export const log_75 = () => console.log("Init 75");
 export const log_76 = () => console.log("Init 76");
+export const log_77 = () => console.log("Init 77");
