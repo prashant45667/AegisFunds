54150514b3297d517b135c5664219df5ed5f5626
prashant45667 <prashantgond724@gmail.com>
2026-08-10 17:00:00 +0000

    Optimize image assets for faster loading (patch #92)

--- frontend/src/utils/logger.ts
+++ frontend/src/utils/logger.ts
@@ -90,3 +90,4 @@ export const log_88 = () => console.log("Init 88");
 export const log_89 = () => console.log("Init 89");
 export const log_90 = () => console.log("Init 90");
 export const log_91 = () => console.log("Init 91");
+export const log_92 = () => console.log("Init 92");
