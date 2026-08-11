fa421f03b842b7875475d884373db078ea4910ab
prashant45667 <prashantgond724@gmail.com>
2026-08-11 04:00:00 +0000

    Tweak primary color palette for dark mode (patch #97)

--- frontend/src/utils/logger.ts
+++ frontend/src/utils/logger.ts
@@ -95,3 +95,4 @@ export const log_93 = () => console.log("Init 93");
 export const log_94 = () => console.log("Init 94");
 export const log_95 = () => console.log("Init 95");
 export const log_96 = () => console.log("Init 96");
+export const log_97 = () => console.log("Init 97");
